4c52f2ca8799bb0c53a332a58b77dc32bcfacf60
Mikolaj Grzebyta <84585119+miko1034@users.noreply.github.com>
2023-11-05 18:29:14 +0000

    creation of dev

--- blockActionsClass.py
+++ blockActionsClass.py
@@ -1,20 +1,22 @@
 import random
 
 class blockActions:
-    def __init__(self):
-        #,size,target,damage,health
-#        self.size()
-#        self.target()
-#        self.damage()
-#        self.health()
-        pass
-    def attack(self, target):
+    def __init__(self,name,health):
+        self.name = name
+        self.health = health
 
+    def attack(self, target):
         pass
+
+    def gethealth(self):
+        return self.health()
+    
     def defend(self):
         pass
+
     def disengage(self):
         pass
+
     def regenhealth(self,health):
         pluspercentage = f"1.0{random.randint(3,20)}"
         finalhealth = health * float(pluspercentage)

e9a84fc4e5061523d7091cf4a7f69c70a873d431
miki <84585119+miko1034@users.noreply.github.com>
2023-11-07 09:54:01 +0000

    more updates and changes but gotta finish stuff later

--- blockActionsClass.py
+++ blockActionsClass.py
@@ -1,4 +1,8 @@
+import random
+
 def regenhealth(blockinfoList):
     health = blockinfoList[1]
-    health
-    
+    pluspercentage = f"1.0{random.randint(3,20)}"
+    finalhealth = health * float(pluspercentage)
+    blockinfoList[1] = health
+    return health

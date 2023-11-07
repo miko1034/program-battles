6931db17847644d5c12e26e7d3a9a52eddc0bee6
Mikolaj Grzebyta <84585119+miko1034@users.noreply.github.com>
2023-11-07 18:19:03 +0000

    started to implement attack function

--- func.py
+++ func.py
@@ -1,5 +1,10 @@
 from block import Block
+from test import block1,block2
 
+
+def attack():
+    pass
+    #work on this next pls :)
 
 #
 #           COMPLETELY IGNORE THIS FILE UNTIL ALL THE BLOCK PROPERTIES
